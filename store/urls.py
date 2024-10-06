4ee22e189c9a647d2b4a1650cfc45fc7153559b3
Irenetitor <irene.martin.campo91@gmail.com>
2024-10-06 21:35:05 +0100

    Adding sample pages for order management

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
 	path('', views.store, name="store-home"),
+	path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
     path('about/', views.store , name='store-about'),
 
     path('menu1/', views.store , name='menu1'),
--- store/views.py
+++ store/views.py
@@ -8,3 +8,10 @@ def store(request):
 	products = Product.objects.all()
 	context = {'products':products}
 	return render(request, 'store/store.html', context)
+
+def cart(request):
+	return render(request, 'store/cart.html')
+
+def checkout(request):
+	return render(request, 'store/checkout.html')
+
