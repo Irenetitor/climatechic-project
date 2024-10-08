a46dbcb2253d6434948a5eac241d86c2cd4211f1
Irenetitor <irene.martin.campo91@gmail.com>
2024-10-08 21:30:40 +0100

    Adding cart functionality for guest and logged in user

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from store.models import Product
+from store.models import Order, OrderItem, Product, ShippingAddress
 
 # Register your models here.
 
 admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
--- store/models.py
+++ store/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.shortcuts import render
+
+from users.models import CustomerProfile
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
@@ -15,3 +18,57 @@ class Product(models.Model):
         except:
             url = ''
         return url
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(CustomerProfile, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return str(self.id)
+    
+    @property
+    def shipping(self):
+        return True
+        
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total 
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(CustomerProfile, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
+
+def store(request):
+    products = Product.objects.all()
+    context = {'products':products}
+    return render(request, 'store/store.html', context)
