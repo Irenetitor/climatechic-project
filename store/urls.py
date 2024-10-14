0db4c0edc0a4800c1a8a3f31b1b354b05a0a8215
Irenetitor <irene.martin.campo91@gmail.com>
2024-10-14 23:06:08 +0100

    Updating about and contact pages

--- store/urls.py
+++ store/urls.py
@@ -5,11 +5,10 @@ urlpatterns = [
 	path('', views.store, name="store-home"),
 	path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
-    path('about/', views.store , name='store-about'),
+    path('about/', views.about , name='store-about'),
+    path('contact/', views.contact , name='store-contact'),
     path('update_item/', views.updateItem, name="update_item"),
     path('process_order/', views.processOrder, name="process_order"),
 
-    path('menu1/', views.store , name='menu1'),
-    path('menu2/', views.store , name='menu2')
 
 ]
